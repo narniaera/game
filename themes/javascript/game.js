ef22edb4c5019cc2750843a516edf5338d59ecc6
郭超 <guochao@guochaodeMacBook-Pro.local>
2017-03-24 23:16:08 +0800

    add

--- themes/javascript/game.js
+++ themes/javascript/game.js
@@ -26,7 +26,7 @@ var Game = function (options) {
 }
 Game.prototype.start = function (data) {
     // 创建应用
-    this.data.app = new PIXLEO.Application(window.innerWidth, window.innerHeight);
+    var app = this.data.app = new PIXLEO.Application(window.innerWidth, window.innerHeight);
     // 创建画布
     this.data.app.insert(document.body);
     // 绘制阴影
@@ -34,13 +34,60 @@ Game.prototype.start = function (data) {
     this.data.app.fillRect('blue',20,20,100,80);
     this.data.app.restore();
     // 填充矩形
+    this.data.app.save();
     this.data.app.fillRect('blue',20,120,100,80);
     this.data.app.restore();
     // 线框矩形
+    this.data.app.save();
     this.data.app.strokeRect('blue',20,220,100,80);
     this.data.app.restore();
     // 清楚区域
+    this.data.app.save();
     this.data.app.fillRect('blue',20,320,100,80);
     this.data.app.clearRect(30,330,80,60)
     this.data.app.restore();
+    // 线性渐变
+    app.save();
+    var grd = this.data.app.createLinearGradient(0,0,300,0,{0 : 'black',1 : 'white'});
+    this.data.app.fillRect(grd,140,20,100,80);
+    app.restore();
+    app.save();
+    // 平铺
+    var img = new Image();
+    img.src = 'themes/image/star.png';
+    img.onload = function () {
+        var pa = app.createPattern(this,'repeat');
+        app.fillRect(pa,140,120,100,80);
+    }
+    app.restore();
+    // 线条样式
+    app.save();
+    app.beginPath();
+    app.lineStyleCap('square');
+    app.moveTo(140,220);
+    app.lineTo(240,300);
+    //app.stroke();
+    app.moveTo(140,300);
+    app.lineTo(240,220);
+    app.stroke();
+    app.restore();
+    // 裁切
+    app.save();
+    // 剪切矩形区域
+    //app.canvas.context.rect(140,320,100,80);
+    //app.canvas.context.stroke();
+    app.rect(140,320,100,80);
+    app.stroke();
+    app.canvas.context.clip();
+    // 在 clip() 之后绘制绿色矩形
+    //app.canvas.context.fillRect('green',140,320,100,80);
+    app.fillRect('blue',140,320,50,30);
+    app.restore();
+    // 曲线
+    app.save();
+    app.beginPath();
+    app.moveTo(260,20);
+    app.quadraticCurveTo(260,100,400,20);
+    app.stroke();
+    app.restore();
 }
--- themes/javascript/pixleo.js
+++ themes/javascript/pixleo.js
@@ -19,9 +19,14 @@
             bgColor: '#ffffff'
         }
         this.canvas = {
+            // 当前画布视图节点
             view: null,
+            // 当前画布临时节点
             element: null,
-            context: null
+            // 画布上下文（画笔）
+            context: null,
+            // 渐变颜色对象
+            grd: null,
         }
         // 调用渲染属性设置
         this._render(width, height, bgColor);
@@ -48,17 +53,17 @@
      */
     window.PIXLEO.Application.prototype.insert = function (element) {
         // 创建htmlElement
-        this.canvas.view = document.createElement('canvas');
-        this.canvas.view.id = 'canvas' + Math.ceil(Math.random() * 10000);
-        this.canvas.view.width = this.render.appWidth;
-        this.canvas.view.height = this.render.appHeight;
+        this.canvas.element = document.createElement('canvas');
+        this.canvas.element.id = 'canvas' + Math.ceil(Math.random() * 10000);
+        this.canvas.element.width = this.render.appWidth;
+        this.canvas.element.height = this.render.appHeight;
         // 添加到页面
-        element.appendChild(this.canvas.view);
-        this.canvas.element = document.getElementById(this.canvas.view.id);
+        element.appendChild(this.canvas.element);
+        this.canvas.view = document.getElementById(this.canvas.element.id);
         // 获取上下文
-        this.canvas.context = this.canvas.element.getContext("2d");
+        this.canvas.context = this.canvas.view.getContext("2d");
         // 创建存储点
-        this.canvas.context.save();
+        this.save();
     }
     /**
      * 设立保存点
@@ -103,8 +108,25 @@
         this.canvas.context.strokeStyle = fillStyle;
         this.canvas.context.strokeRect(x, y, width, height);
     }
-    window.PIXLEO.Application.prototype.clearRect = function (x,y,width,height) {
-        this.canvas.context.clearRect(x,y,width,height);
+    /**
+     * 创建矩形
+     * @param x         x坐标 起始点
+     * @param y         y坐标 起始点
+     * @param width     宽度
+     * @param height    高度
+     */
+    window.PIXLEO.Application.prototype.rect = function (x, y, width, height) {
+        this.canvas.context.rect(x, y, width, height);
+    }
+    /**
+     * 清空某块区域
+     * @param x         x坐标
+     * @param y         y坐标
+     * @param width     宽度
+     * @param height    高度
+     */
+    window.PIXLEO.Application.prototype.clearRect = function (x, y, width, height) {
+        this.canvas.context.clearRect(x, y, width, height);
     }
     /**
      * 设置阴影
@@ -134,10 +156,15 @@
      * @param y0    y开始坐标
      * @param x1    x结束坐标
      * @param y1    y结束坐标
+     * @param option    渐变点{0 : 'black', 1 : 'white', n : 'm' ....} 0 - 1之间
      * @returns {CanvasGradient}    线性渐变对象
      */
-    window.PIXLEO.Application.prototype.linearGradient = function (x0, y0, x1, y1) {
-        return this.canvas.context.createLinearGradient(x0, y0, x1, y1);
+    window.PIXLEO.Application.prototype.createLinearGradient = function (x0, y0, x1, y1, option) {
+        this.canvas.grd = this.canvas.context.createLinearGradient(x0, y0, x1, y1);
+        for (var i in option) {
+            this.canvas.grd.addColorStop(i, option[i]);
+        }
+        return this.canvas.grd;
     }
     /**
      * 创建放射状|圆形渐变
@@ -149,9 +176,103 @@
      * @param r1    结束的圆半径
      * @returns {CanvasGradient}
      */
-    window.PIXLEO.Application.prototype.radialGradient = function (x0, y0, r0, x1, y1, r1) {
-        return this.canvas.context.createRadialGradient(x0, y0, r0, x1, y1, r1);
+    window.PIXLEO.Application.prototype.createRadialGradient = function (x0, y0, r0, x1, y1, r1) {
+        this.canvas.grd = this.canvas.context.createRadialGradient(x0, y0, r0, x1, y1, r1);
+        for (var i in option) {
+            this.canvas.grd.addColorStop(i, option[i]);
+        }
+        return this.canvas.grd;
     }
-
-
+    /**
+     * 向指定的方向内重复指定的元素
+     * @param img       规定要使用的图片、画布或视频元素
+     * @param repeat    "repeat|repeat-x|repeat-y|no-repeat"    默认repeat
+     */
+    window.PIXLEO.Application.prototype.createPattern = function (img, repeat) {
+        return this.canvas.context.createPattern(img, repeat);
+    }
+    /**
+     * 创建路径
+     */
+    window.PIXLEO.Application.prototype.beginPath = function () {
+        this.canvas.context.beginPath();
+    }
+    /**
+     * 闭合路径
+     */
+    window.PIXLEO.Application.prototype.closePath = function () {
+        this.canvas.context.closePath();
+    }
+    /**
+     * 绘制线条起始点
+     * @param x x坐标
+     * @param y y坐标
+     */
+    window.PIXLEO.Application.prototype.moveTo = function (x, y) {
+        this.canvas.context.moveTo(x, y);
+    }
+    /**
+     * 绘制线条线路
+     * @param x x坐标
+     * @param y y坐标
+     */
+    window.PIXLEO.Application.prototype.lineTo = function (x, y) {
+        this.canvas.context.lineTo(x, y);
+    }
+    /**
+     * 在画布区域内创建裁切点
+     *      创建裁切区域
+     *      context.rect(50,20,200,120);
+     *      context.stroke();
+     *      绘制裁切
+     *      context.clip();
+     *      绘制需要剪裁的区域
+     *      context.fillStyle="green";
+     *      context.fillRect(0,0,150,100);
+     */
+    window.PIXLEO.Application.prototype.clip = function () {
+        this.canvas.context.clip();
+    }
+    /**
+     * 设置线条末端(两端)样式
+     * @param style =
+     *          butt 默认。向线条的每个末端添加平直的边缘
+     *          round 向线条的每个末端添加圆形线帽
+     *          square 向线条的每个末端添加正方形线
+     */
+    window.PIXLEO.Application.prototype.lineStyleCap = function (style) {
+        this.canvas.context.lineCap = style;
+    }
+    /**
+     * 设置两条线交汇时的边角的类型
+     * @param style =
+     *          bevel   创建斜角
+     *          round   创建圆角
+     *          miter   默认。创建尖角
+     *                  miterLimit=number 规定最大斜接长度 说明: > 内角空隙处的坡度
+     */
+    window.PIXLEO.Application.prototype.lineJoin = function (style) {
+        this.canvas.context.lineJoin = style;
+    }
+    /**
+     * 设置线条宽度
+     * @param width number
+     */
+    window.PIXLEO.Application.prototype.lineWidth = function (width) {
+        this.canvas.context.lineWidth = width;
+    }
+    /**
+     * 绘制二次贝塞尔曲线    配合路径和线条使用
+     * @param x0    x起始点
+     * @param y0    y起始点
+     * @param x1    x结束点
+     * @param y1    y结束点
+     *                  context.beginPath()
+     *                  context.moveTo(100,100)
+     *                  context.quadraticCurveTo(x0,y0,x1,y1)
+     *                  context.stroke()
+     */
+    window.PIXLEO.Application.prototype.quadraticCurveTo = function (x0,y0,x1,y1) {
+        this.canvas.context.quadraticCurveTo(x0,y0,x1,y1);
+    }
 }())
